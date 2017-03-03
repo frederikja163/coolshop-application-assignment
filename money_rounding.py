2ebbb9dcf32ec1bee38f59e6be24c17529402042
Emil Jensen <emsj@entertainment-trading.com>
2017-03-03 15:51:51 +0100

    Add task for rounding of money

---
+++ money_rounding.py
@@ -0,0 +1,7 @@
+def get_price_without_vat(price_to_show, vat_percent):
+    raise NotImplementedError()
+
+
+def get_price_without_vat_from_other_valuta(conversion_rate, origin_price,
+                                            origin_vat, other_vat):
+    raise NotImplementedError()
