3742545d6b2c13198bd1ecb3f7c675a8e6dad948
Emil Jensen <emsj@entertainment-trading.com>
2017-03-07 13:10:28 +0100

    Add file with premium dates task

---
+++ premium_and_date.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+
+def get_first_payment_date(signup_date, previous_premium):
+    raise NotImplementedError()
+
+
+def get_end_date(sign_up_date, cancel_date):
+    raise NotImplementedError()
+
+
+def get_billing_day(signup_date):
+    raise NotImplementedError()
